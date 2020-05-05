ecf27b65267cfcff0b78b1abd49365e7faa14e4d
Dima Antonenko <daemondw@gmail.com>
2020-05-05 23:37:20 +0200

    Makefile

--- public/index.php
+++ public/index.php
@@ -11,4 +11,22 @@ $app->addErrorMiddleware(true, true, true);
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
+
+$app->get('/companies', function ($request, $response) {
+    return $response->write('GET /companies');
+});
+
+$app->post('/companies', function ($request, $response) {
+    return $response->write('POST /companies');
+});
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+
+
 $app->run();
